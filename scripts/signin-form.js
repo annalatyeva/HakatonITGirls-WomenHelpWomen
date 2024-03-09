56b978eb03de297028acf2bbc2487fc485b7c8b6
annalatyeva <anysha_s@mail.ru>
2024-03-09 17:56:49 +0500

    registration with localtorage

--- scripts/signin-form.js
+++ scripts/signin-form.js
@@ -1,4 +1,30 @@
-let users = [];
+function setInitialUsersData() {
+    const initialUsers = [
+        {
+            login: 'user1',
+            password: 'password1',
+            strengths: ['strength1', 'strength2'],
+            description: 'Description for user1'
+        },
+        {
+            login: 'user2',
+            password: 'password2',
+            strengths: ['strength3'],
+            description: 'Description for user2'
+        }
+    ];
+
+    localStorage.setItem('herHelpHubUsers', JSON.stringify(initialUsers));
+}
+
+function saveUsersToLocalStorage(users) {
+    localStorage.setItem('herHelpHubUsers', JSON.stringify(users));
+}
+
+function getUsersFromLocalStorage() {
+    const users = localStorage.getItem('herHelpHubUsers');
+    return users ? JSON.parse(users) : [];
+}
 
 function submitForm() {
     const login = document.getElementById('login').value;
@@ -6,8 +32,8 @@ function submitForm() {
     const description = document.getElementById('description').value;
 
     const strengths = [];
-    const strengthCheckboxes = document.querySelectorAll('input[name="strength"]:checked');
-    strengthCheckboxes.forEach(checkbox => {
+    const checkedSrengths = document.querySelectorAll('input[name="strength"]:checked');
+    checkedSrengths.forEach(checkbox => {
         strengths.push(checkbox.value);
     });
 
@@ -18,9 +44,11 @@ function submitForm() {
         description: description
     };
 
+    let users = getUsersFromLocalStorage();
+
     users.push(user);
 
-    console.log(users);
+    saveUsersToLocalStorage(users);
 
     document.getElementById('login').value = '';
     document.getElementById('password').value = '';
@@ -28,4 +56,12 @@ function submitForm() {
     strengthCheckboxes.forEach(checkbox => {
         checkbox.checked = false;
     });
+
+    console.log(users);
 }
+
+if (!localStorage.getItem('herHelpHubUsers')) {
+    setInitialUsersData();
+}
+
+
