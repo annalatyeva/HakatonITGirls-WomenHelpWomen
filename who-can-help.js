6d5a9f123a51157a7d9005913d840735f8ddca9f
annalatyeva <anysha_s@mail.ru>
2024-03-09 20:03:33 +0500

    full

--- who-can-help.js
+++ who-can-help.js
@@ -1,14 +1,21 @@
 
-function searchUsersByStrength(с) {
-    function getUsersByHelpCategory(category) {
-        return initialUsers.filter(user => user.helpAreas.includes(category));
-    }
-    event.preventDefault();
-    const selectedCategory = document.getElementById('category').value;
-    const users = localStorage.getItem('herHelpHubUsers');
-    console.log(selectedCategory);
-    const filteredSpecialists = users.filter(user => user.helpAreas.includes(selectedCategory));
+// function searchUsersByStrength(с) {
+//         return initialUsers.filter(user => user.helpAreas.includes(category));
+//     }
+//     event.preventDefault();
+//     const selectedCategory = document.getElementById('category').value;
+//     const users = localStorage.getItem('herHelpHubUsers');
+//     console.log(selectedCategory);
+//     const filteredSpecialists = users.filter(user => user.helpAreas.includes(selectedCategory));
     
+
+let resultsContainer = document.getElementById('results');
+function searchUsersByStrength() {
+    const category = document.getElementById('list-specialists').value;
+    console.log(category);
+    const users = JSON.parse(localStorage.getItem('herHelpHubUsers'));
+    const filteredSpecialists = users.filter(user => user.helpAreas.includes(category));
+    console.log(users);
         filteredSpecialists.forEach(specialist => 
             {let card = document.createElement('div');
         card.classList.add('card');
@@ -17,3 +24,5 @@ function searchUsersByStrength(с) {
         resultsContainer.appendChild(card);
     });
 }
+
+
